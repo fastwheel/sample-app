1fb85fa2a5ad1cf163ed326b6b5ee4dc28f617d1
Philip Chang <fastwheel@gmail.com>
2010-11-14 02:06:55 -0500

    added sign in's

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 SampleApp::Application.routes.draw do
   
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   match '/',            :to => "pages#home"
   match '/contact',     :to => "pages#contact"
   match '/about',       :to => "pages#about"
   match '/help',        :to => "pages#help"
   match '/signup',      :to => "users#new"
+  match '/signin',	:to => "sessions#new"
+  match '/signout',	:to => "sessions#destroy"
   root                   :to => "pages#home"
   
   get "pages/home"
